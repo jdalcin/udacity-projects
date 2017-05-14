c213d638ab80ed76c3eea0ea887c87f5969fb5f5
jdalcin <jbdalcin@gmail.com>
2017-05-14 12:43:37 -0400

    Update last half of website

--- item-catalog/setup_database.py
+++ item-catalog/setup_database.py
@@ -21,17 +21,22 @@ class Item(Base):
 
 	id = Column(Integer, primary_key = True)
 	name = Column(String(250), nullable = False)
+	category = Column(String(250), nullable = False)
 	picture = Column(String(250))
 	description = Column(Text)
 	date_uploaded = Column(DateTime(timezone = True), nullable = False, server_default = func.now())
 	user_id = Column(Integer, ForeignKey('user.id'))
+
 	user = relationship(User)
+	item_owner_name = None
 
 	@property
 	def serialize(self):
 		return {
 			'name': self.name,
+			'category': self.category,
 			'picture': self.picture,
 			'description': self.description,
-			'date_uploaded': self.date_uploaded
+			'date_uploaded': str(self.date_uploaded),
+			'item_owner': self.item_owner_name
 		}
